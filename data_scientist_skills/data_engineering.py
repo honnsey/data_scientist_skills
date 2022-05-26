ac0955f30b8fd30051e18f9f4bd7dcd50c178947
Phuong Nguyen <honnsey@gmail.com>
2022-05-26 09:54:58 +0930

    replacing with nan, KNNimputer, categorize by level and stream added

--- data_scientist_skills/data_engineering.py
+++ data_scientist_skills/data_engineering.py
@@ -1,4 +1,9 @@
-def level_encoder(df):
+import pandas as pd
+import numpy as np
+from sklearn.impute import KNNImputer
+
+
+def known_level_encoder(df):
     '''Encoding levels based on job titles.
     Return original df plus 3 level-encoded features.
     '''
@@ -19,13 +24,15 @@ def level_encoder(df):
 
 def job_type_encoder(df):
     '''Returns encoding for three streams:
-    analyst, engineer and scientist
+    analyst, engineer and scientist based on job title
     '''
 
     titles = ['analyst', 'engineer', 'scien']
 
+    # Include "encoded" in column name, as keywords extracted from job
+    # description may include words in titles
     for title in titles:
-        df[title] = df['cleaned_title'].str.contains(title)\
+        df[f'encoded {title}'] = df['cleaned_title'].str.contains(title)\
                     .apply(lambda x: 1 if x is True else 0)
     return df
 
@@ -76,3 +83,70 @@ def process_salary_estimate(dataframe, only_mean = True):
     temp_df.drop(columns = 'salary_estimate', inplace = True)
 
     return temp_df
+
+def replace_with_NaN(df):
+    '''Replace unknown job title with NaN.
+    Data prepartion for KNNImputer.
+    Return the entire dataframe with updated values'''
+
+    columns = [('title', 'junior'), ('title', 'senior'), ('title', 'mid-level')]
+
+    filters = (df['title', 'junior'] == 1)& (df['title', 'senior'] == 0)& (df['title', 'mid-level'] == 0)| \
+            (df['title', 'junior'] == 0)& (df['title', 'senior'] == 1)& (df['title', 'mid-level'] == 0)| \
+            (df['title', 'junior'] == 0)& (df['title', 'senior'] == 0)& (df['title', 'mid-level'] == 1)
+
+    df[columns] = df[columns].where(filters)
+
+    return df
+
+def imputer(df, n_neighbors= 5):
+    '''Impute Nan Values in dataframe
+    Return dataframe with updated values'''
+
+    # Drop first 13 columns
+    # dropped columns = ['job_title', 'job_description', 'rating', 'company_name', 'location',
+    #    'headquarters', 'size', 'founded', 'type_of_ownership', 'industry',
+    #    'sector', 'cleaned_description', 'cleaned_title'],
+
+    X = df.drop(columns= df.columns[0:13])
+
+    imputer = KNNImputer(n_neighbors= n_neighbors)
+    X_imputed = imputer.fit_transform(X)
+
+    return pd.DataFrame(X_imputed, columns= X.columns)
+
+def level_encoder_post_impute(df, threshold= 0.7):
+    '''Categorize seniority level based on imputed value (probability)
+    return dataframe with 0 or 1 encoded levels'''
+
+    columns = [('title', 'junior'), ('title', 'senior')]
+    for column in columns:
+        df[column] = df[column].apply(lambda x: 1 if x >= threshold else 0)
+
+    mid_level_filter = (df[columns[0]] < threshold) & (df[columns[1]] < threshold)
+    df['title', 'mid-level'] = np.where(mid_level_filter, 1, 0)
+
+    return df
+
+def get_position(df):
+    '''
+    Return the Category (level and stream) as a Series
+    Use to visualise dataframe - to compare with unsupervised model results
+    '''
+
+    levels = [('title', 'junior'), ('title', 'senior'), ('title', 'mid-level')]
+    streams = ['encoded analyst', 'encoded engineer', 'encoded scien']
+
+    def get_level(row):
+        '''Return job level: junior, mid-level or senior'''
+        return levels[np.argmax(row.values)][1]
+
+    def get_stream(row):
+        '''Return stream: scientist, analyst, engineer'''
+        return streams[np.argmax(row.values)]
+
+    Category = df[streams].apply(get_stream, axis= 1) \
+                        + ' ' \
+                        + df[levels].apply(get_level, axis= 1)
+
+    return Category
