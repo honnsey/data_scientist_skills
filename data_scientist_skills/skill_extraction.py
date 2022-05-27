99f7cab78f84bfdc653e1f7d08adce3cae1303b0
Nicholas Hale <n.a.hale@hotmail.com>
2022-05-27 17:09:50 +1000

    Updated docstring for tdidf()

--- data_scientist_skills/skill_extraction.py
+++ data_scientist_skills/skill_extraction.py
@@ -10,12 +10,13 @@ def remove_more_stopwords(text):
     clean_text.replace(',', '')
     return clean_text
 
-def tdidf(X, min_df=0.1, max_df=0.6, results_df=False, transform=False):
-    """Performs TD-IDF on pd Series. Results_df=True to return table of results. """
+def tdidf(X, min_df=0.01, max_df=0.8, results_df=False, transform=False):
+    """Performs TF-IDF on pd Series.
+        Transform=True to return vectorizer for use on input in model.py>init_vectorizer
+        Results_df=True to return table of results. """
     tfidf = TfidfVectorizer(ngram_range=(1,2), min_df=min_df, max_df=max_df)
 
     if transform:
-        # tfidf.fit(X)
         return tfidf
 
     X_vec = tfidf.fit_transform(X)
