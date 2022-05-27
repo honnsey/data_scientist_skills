4ba6514eae30c226270382b7446bb513deba8480
Nicholas Hale <n.a.hale@hotmail.com>
2022-05-27 16:33:55 +1000

    changed tdidf() to return TfidfVectorizer early for use in model preproc

--- data_scientist_skills/skill_extraction.py
+++ data_scientist_skills/skill_extraction.py
@@ -10,9 +10,14 @@ def remove_more_stopwords(text):
     clean_text.replace(',', '')
     return clean_text
 
-def tdidf(X, min_df=0.1, max_df=0.6, results_df=False):
+def tdidf(X, min_df=0.1, max_df=0.6, results_df=False, transform=False):
     """Performs TD-IDF on pd Series. Results_df=True to return table of results. """
     tfidf = TfidfVectorizer(ngram_range=(1,2), min_df=min_df, max_df=max_df)
+
+    if transform:
+        # tfidf.fit(X)
+        return tfidf
+
     X_vec = tfidf.fit_transform(X)
     res = pd.DataFrame(X_vec.toarray(), columns = tfidf.get_feature_names_out())
     if results_df:
