e2e4bb17b414bccbb4a905c80c84e205d844c633
Nicholas Hale <n.a.hale@hotmail.com>
2022-05-30 17:03:35 +1000

    Implemented over-sampling method to even out number of each target. Model now trains on equal amounts of each classification

--- data_scientist_skills/model.py
+++ data_scientist_skills/model.py
@@ -2,28 +2,33 @@ from data_scientist_skills.data import get_data, clean_dataframe
 from data_scientist_skills.data_engineering import process_dataframe, label_encoder
 from data_scientist_skills.skill_extraction import tdidf, remove_more_stopwords
 from sklearn.svm import SVC
+from imblearn.over_sampling import SMOTEN
 import pandas as pd
 
 def data_prep(return_x=False):
-    """Used when instantiating Model class"""
+    """Used when instantiating Model class.
+    Prepares data for model training"""
     # Data preparation for model training
     df = get_data()
     clean_df = clean_dataframe(df)
     processed_df = process_dataframe(clean_df)
 
     # X and y
-    X = clean_df['cleaned_description'] + df['cleaned_title']
+    X_orig = clean_df['cleaned_description'] + df['cleaned_title']
+    y_orig = label_encoder(processed_df, 'Category')['lenc_Category']
+
+    # Over-Sampling method for training
+    X, y = SMOTEN().fit_resample(pd.DataFrame(X_orig), y_orig)
 
     # Returns X for fitting vectorizer, to transform input
     if return_x:
-        return X
+        return X[0]
 
-    X_svc = tdidf(X, results_df=True)
-    y_svc = label_encoder(processed_df, 'Category')['lenc_Category']
-    return X_svc, y_svc
+    X = tdidf(X[0], results_df=True)
+    return X, y
 
 def model_prep():
-    """Used in Model class constructor to fit model on training data"""
+    """Used in Model class constructor to train model in preparation for prediction"""
     # Get X & y
     X_svc, y_svc = data_prep()
     # Model
@@ -33,7 +38,7 @@ def model_prep():
     return model_svc
 
 def init_vectorizer():
-    """Used in Model class constructor to initialize vectorizer for input"""
+    """Used in Model class constructor to initialize vectorizer to vectorize input"""
     X = data_prep(return_x=True)
     vectorizer = tdidf(X, transform=True)
     vectorizer.fit(X)
@@ -46,6 +51,7 @@ class Model:
         self.vectorizer = init_vectorizer()
 
     def prep_input(self, input):
+        """Vectorizes the input for use in prediction"""
         # Vectorize input >>> Pick's out key words from input and vectorizes
         vectorized_input = self.vectorizer.transform(input)
 
@@ -55,6 +61,7 @@ class Model:
         return input_dataframe
 
     def predict(self, input):
+        # Makes input iterable
         if type(input) == str:
             input = [input]
         input_ready = self.prep_input(input)
