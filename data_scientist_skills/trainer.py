2f71a6e6437fcb05985497f861b113ef6eb23be5
Joseph Peguero <joepeguero@gmail.com>
2022-06-01 18:47:01 +0900

    Modified to also save the new jobs recommender model. Cleaned up previous comments, hopefully increasing readabilty

--- data_scientist_skills/trainer.py
+++ data_scientist_skills/trainer.py
@@ -1,13 +1,13 @@
 import joblib
 from google.cloud import storage
 from termcolor import colored
-from data_scientist_skills import model
+from data_scientist_skills import model, recommended_jobs
 
-### Implement params.py so that the gcp stuff isn't defined in every .py
-
+### Implement params.py so that the gcp information doesn't need to be defined
+### in every .py -JP
 BUCKET_NAME = 'skills_for_a_data_scientist'
 
-###Not needed because of how model is invoked
+###Not needed because of how model is currently invoked -JP
 # BUCKET_TRAIN_DATA_PATH = 'data/raw_data/DataAnalyst.csv'
 # BUCKET_TRAIN_DATA_PATH_2 = 'data/raw_data/DataScientist.csv'
 
@@ -23,41 +23,42 @@ MODEL_VERSION = 'v1'
 
 STORAGE_LOCATION = f'models/{MODEL_NAME}/{MODEL_VERSION}/model.joblib'
 
-# def upload_model_to_gcp():
-#     """Uploads model.joblib to GCP, currently unused."""
-#     client = storage.Client()
-#     bucket = client.bucket(BUCKET_NAME)
-#     blob = bucket.blob(STORAGE_LOCATION)
-#     blob.upload_from_filename('model.joblib')
+def upload_model_to_gcp():
+    """Uploads model.joblib to GCP, currently unused."""
+    client = storage.Client()
+    bucket = client.bucket(BUCKET_NAME)
+    blob = bucket.blob(STORAGE_LOCATION)
+    blob.upload_from_filename('model.joblib')
 
-#There is currently no unique naming so any previously saved model is overwritten
-#Take care in case you want to save another model
+### This method could be rewritten to handle an arbritary amount of python
+### objects -JP
+#Take care in case you want to save another model, as written this method
+#overwrites any saved model.joblib of the same name
 def save_model(reg):
-    """method that saves the model into a .joblib file and uploads it on Google
+    """method that saves the model(s) into a .joblib file and *could* upload it on Google
     Storage /models folder use joblib library and google-cloud-storage"""
 
     #saving the trained model locally
-    joblib.dump(reg, 'model.joblib')
-    print(colored("model.joblib saved locally", "green"))
+    joblib.dump(reg, f'{reg.nickname}_model.joblib')
+    print(colored(f"{reg.nickname}_model.joblib' saved locally", "green"))
 
     # Saves trained model to GCP. Currently unused.
     # upload_model_to_gcp()
-    # print(f"uploaded model.joblib to gcp cloud storage under \n => {STORAGE_LOCATION}")
-
+    # print(colored(f"uploaded{reg.nickname}_model.joblib' \
+        # to gcp cloud storage under \n => {STORAGE_LOCATION}", "blue"))
 
 
 if __name__ == '__main__':
-    # get training data from GCP bucket ####file path needs to be updated
-    ###With restructured code we could have more flexible definition here
-    inst_model = model.Model()
+    # Runs the imported class objects and saves them via save_model method
+    ### With restructured code we could have more flexible definitions here
+    ### e.g. where/how the data is imported and processed, enabling GCP training-JP
 
-    # # preprocess data
-    # step doen in Model instantiation
+    #Instantiates the model objects
+    svc_and_vec_model = model.Model()
+    jobs_rec_model = recommended_jobs.Recommend_Jobs()
 
-    # # train model (locally if this file was called through the run_locally command
-    # # or on GCP if it was called through the gcp_submit_training, in which case
-    # # this package is uploaded to GCP before being executed)
-    # step done in Model instantiation
 
-    # save trained model to GCP bucket (whether the training occured locally or on GCP)
-    save_model(inst_model)
+    #Saves trained models locally (and GCP bucket if lines in save_model
+    #are uncommented)
+    save_model(svc_and_vec_model)
+    save_model(jobs_rec_model)
